f3e491d8f4c84019a730b4dc223f466b1333fa9f
jorgegoco <jorgegoco70@gmail.com>
2023-05-31 19:36:05 +0200

    Get the user logged in data tothe view

--- src/components/Room.js
+++ src/components/Room.js
@@ -2,14 +2,18 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import fetchRoom from '../redux/room/fetchRoom';
+import fetchUser from '../redux/user/fetchUser';
+
 import Navigate from './Navigation';
 
 const Room = () => {
   const dispatch = useDispatch();
   useEffect(() => {
+    dispatch(fetchUser());
     dispatch(fetchRoom());
   }, [dispatch]);
   const roomData = useSelector((state) => state.room.data);
+  const userData = useSelector((state) => state.user.data);
 
   return (
     <div className="container">
@@ -18,6 +22,7 @@ const Room = () => {
         <div className="header-div">
           <h1>Rooms</h1>
           <p>Please Select a room below</p>
+          <div className="user-info">{userData.name}</div>
         </div>
         <div className="rooms row" style={{ marginTop: '40px' }}>
           {roomData.map((room) => (
@@ -32,21 +37,6 @@ const Room = () => {
                   </div>
                 </Link>
               </div>
-
-              {/* <div className='card'>
-              <Link to={`/rooms/${room.id}`}> */}
-              {/* Room ID:
-              {room.id} */}
-              {/* <h3 className="room-title">{room.name}</h3>
-                <div>
-                  <img src={room.photo} alt="Room Pic" className="room-img" />
-                </div>
-                <p className="room-info">{room.description}</p> */}
-              {/* {room.city} */}
-              {/* {room.price} */}
-              {/* </Link>
-              </div> */}
-
             </div>
           ))}
         </div>
